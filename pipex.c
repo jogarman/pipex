d69be78d40f977c6eeb03c599961641fefaad968
Jose Garcia Manzanaro <jgarcia3@c1r8s6.42madrid.com>
2024-04-22 00:32:51 +0200

    debo comprender  qué es infile

--- pipex.c
+++ pipex.c
@@ -6,36 +6,50 @@
 /*   By: jgarcia3 <jgarcia3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 20:38:04 by jgarcia3          #+#    #+#             */
-/*   Updated: 2024/04/21 22:30:05 by jgarcia3         ###   ########.fr       */
+/*   Updated: 2024/04/22 00:32:14 by jgarcia3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
 //Notas:
-/* Debo de recibir el argumento como una sola cadena, luego hacer el split para dividirla
-y el resultado mandarlo a execve */
-// implementar el split de mi libreria
+
+/*
+1-Para que puñetas es infile????
+2- añadirr el segundo argumento con el pipe para termminar de comprender como funciona
+3- añadir todos llos  comandos
+*/
+
+//argv[1] = infile
+//argv[2] = comando
 
 int	main(int arg, char* argv[])
 {
 	int		fd_read;
 	int		fd_write;
-	int		pid;
+	int		pid = 0;
 	char	*arg_execve[3];
+	char	*comando;
+	char	**execve_args;
 
-	fd_read = open("infile", O_RDONLY);
+
+	execve_args = ft_split(argv[2], ' ');
+	ft_printf("%s\n", execve_args[0]);
+	ft_printf("%s\n", execve_args[1]);
+
+	//ft_printf("%s\n", args[1]);
+
+/* 	fd_read = open("infile", O_RDONLY);
 	fd_write = open("outfile", O_WRONLY);
-	pid = fork();
+	pid = fork(); */
 
-	(void)argv;
-	arg = 0;
-	if (fd_read < 0 || fd_write < 0 || pid < 0)
-		return -1;
+/* 	if (fd_read < 0 || fd_write < 0 || pid < 0)
+		return -1; */
 	if (pid == 0)
 	{
+
 		//ft_printf("proceso hijo\n");
-		execve("/bin/ls", arg_execve, NULL);
+		execve("/bin/ls", execve_args, NULL);
 	}
 	else
 		//ft_printf("proceso padre\n");
